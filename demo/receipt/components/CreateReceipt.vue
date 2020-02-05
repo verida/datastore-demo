d4d36aa1b7f67d1ab5ad1eea63cafd61c24778dd
antonvakulenko93 <anton.vakulenko93@gmail.com>
2020-02-06 00:42:18 +0200

    added output of the form fields

--- demo/receipt/components/CreateReceipt.vue
+++ demo/receipt/components/CreateReceipt.vue
@@ -1,15 +1,19 @@
 <template>
     <b-modal id="create-receipt" title="Create Receipt" hide-footer>
         <ValidationObserver v-slot="{ invalid }">
-            <!--<ValidationProvider v-slot="{ errors }" rules="did">
-                <b-form-input class="form-control" name="did" aria-describedby="did-error" v-model="did"
-                              :state="!did ? null : !errors[0]" />
+            <ValidationProvider v-slot="{ errors }"
+                                rules="did"
+                                v-for="(item, key) in data" :key="key">
+                <label>{{attributes[key].title}}</label>
+                <b-form-input class="form-control"
+                              :name="key" aria-describedby="did-error" v-model="data[key]"
+                              :state="!data[key] ? null : !errors[0]" />
                 <b-form-invalid-feedback id="did-error">
                     {{ errors[0] }}
                 </b-form-invalid-feedback>
-            </ValidationProvider>-->
-            <b-button @click="confirm" :disabled="invalid" block variant="success">
-                Confirm
+            </ValidationProvider>
+            <b-button @click="create" :disabled="invalid" block variant="success">
+                Create
             </b-button>
         </ValidationObserver>
     </b-modal>
@@ -25,7 +29,8 @@ export default {
       return {
           data: {},
           attributes: {},
-          title: null
+          title: null,
+          category: 'shopping/receipt'
       }
     },
     computed: {
@@ -36,7 +41,7 @@ export default {
     },
     methods: {
         async init () {
-            const { title, properties } =  await this.create('shopping/receipt')
+            const { title, properties } =  await this.create(this.category)
 
             this.title = title
             this.data = {}
@@ -45,6 +50,13 @@ export default {
             Object.keys(properties).forEach(key => {
                 this.$set(this.data, key, null)
                 this.$set(this.attributes, key, properties[key])
+            })
+        },
+        async create () {
+            const store = await window.veridaApp.openDatastore(this.category)
+            await store.save({
+                name: this.data.name,
+                ...this.data
             })
         }
     }
