137d98aac86d1e7d9737d917c3e6ab7356125f89
antonvakulenko93 <anton.vakulenko93@gmail.com>
2020-02-11 20:06:50 +0200

    fixed component rendering

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -23,15 +23,9 @@
         </b-col>
       </b-row>
       <hr />
-      <b-row v-if="loaded">
-        <b-col sm="12" v-for="key in collections" :key="key">
-          <h5 class="my-4 text-info">{{ key | title }}</h5>
-          <b-table hover :items="list[key]" :fields="headers[key]" />
-        </b-col>
-      </b-row>
-      <b-row v-else>
-        <b-col sm="12"> Loading ... </b-col>
-      </b-row>
+      <documents ref="documents"
+        :collections="collections"
+        :loaded="loaded" />
       <create-modal :did="recipient" />
     </template>
     <recepient-did @update-address="updateAddress" />
@@ -39,9 +33,10 @@
 </template>
 
 <script>
-import CreateModal from './CreateModal';
+import CreateModal from './CreateModal'
 import RecepientDid from './RecepientDid'
 import DidStatistics from './DidStatistics'
+import Documents from './Documents'
 
 import {
   connectVerida,
@@ -52,9 +47,6 @@ import {
   bind
 } from '@src/helpers/VeridaTransmitter'
 
-import { createNamespacedHelpers } from 'vuex'
-const { mapGetters: mapSchemaGetters } = createNamespacedHelpers('schema')
-
 export default {
   name: 'Layout',
   props: [
@@ -62,60 +54,20 @@ export default {
     'title',
   ],
   components: {
+    Documents,
     DidStatistics,
     CreateModal,
     RecepientDid
   },
-  filters: {
-    title: (str) => {
-      return str
-        .split('/')
-        .map(item => item.charAt(0).toUpperCase() + item.slice(1))
-        .join(' ')
-    }
-  },
   data () {
     return {
-      loaded: false,
-
       address: null,
       recipient: null,
-
-      headers: {},
-      store: {},
-      list: {}
+      loaded: false
     }
   },
-  computed: {
-    ...mapSchemaGetters(['fields']),
-  },
   methods: {
-    async initDatastore () {
-      this.store = {}
-      this.list = {}
-      this.headers = {}
-
-      for(let i = 0; i < this.collections.length; i++) {
-        const key = this.collections[i]
-        const datastore = await window.veridaApp.openDatastore(key)
-
-        this.$set(this.list, key, [])
-        this.$set(this.store, key, datastore)
-        this.$set(this.headers, key, [])
-
-        await this.initList(key)
-        await this.enableWatcher(key, 'updateList')
-      }
-    },
-    async updateList (key) {
-      this.list[key] = await this.store[key].getMany()
-    },
-    async initList (key) {
-      this.list[key] = await this.store[key].getMany()
-      const { properties } = await this.fields(key)
-      this.headers[key] = Object.keys(properties)
-    },
-    async updateAddress () {
+    async updateAddress() {
       await bind(this.updateAddress, this.disconnect)
       await connectVerida()
 
@@ -123,7 +75,8 @@ export default {
       this.address = await getAddress()
       this.recipient = getRecipient()
 
-      await this.initDatastore()
+      await this.$nextTick()
+      await this.$refs.documents.initDatastore()
 
       this.loaded = true
     },
@@ -131,22 +84,12 @@ export default {
       this.address = null
       this.recipient = null
       logout()
-    },
-    async enableWatcher (category) {
-      const database = await this.store[category].getDb()
-      const instance = await database.getInstance()
-
-      instance.changes({
-        since: 'now',
-        live: true,
-        include_docs: true
-      }).on('change', async () => await this.updateList(category))
     }
   },
-  beforeMount () {
+  async mounted () {
     const did = isConnected()
     if (did) {
-      this.updateAddress()
+      await this.updateAddress()
     }
   }
 }
