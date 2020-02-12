f80e573088c78e66b747dba3060caba61a328492
antonvakulenko93 <anton.vakulenko93@gmail.com>
2020-02-12 13:24:48 +0200

    added animation

--- projects/receipt/views/Landing.vue
+++ projects/receipt/views/Landing.vue
@@ -3,7 +3,9 @@
         <video autoplay muted class="landing" loop>
             <source src="@src/assets/video/landing.mp4" type="video/mp4">
         </video>
-
+        <div class="landing__btn">
+            <img src="@src/assets/img/verida-logo.svg" />
+        </div>
     </div>
 </template>
 
