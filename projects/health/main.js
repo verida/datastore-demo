0f2a8bb1a8abfd6772f31217f26018dcd2f8c8a6
antonvakulenko93 <anton.vakulenko93@gmail.com>
2020-02-26 21:11:29 +0200

    added the `health` demo project

---
+++ projects/health/main.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+
+import App from './App'
+import router from '@src/router'
+import store from '@src/store'
+
+import * as _ from 'underscore'
+import '@src/plugins'
+
+window._ = _
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
+
+import '@/assets/scss/styles.scss'
---
+++ projects/health/views/Home.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="health__body">
+    <div class="health__body--gradient">
+      <div class="health__body--container">
+        <div class="health__navigation">
+          <div class="health__navigation--logo">
+            <img src="../assets/logo.png" />
+            <span class="health__navigation--text"> Patient Medical Data </span>
+          </div>
+          <div class="health__navigation-actions">
+            <div>
+              <div v-for="action in navigation" class="action__item">
+                {{ action }}
+              </div>
+            </div>
+            <div class="action__item">
+              Logout
+            </div>
+          </div>
+        </div>
+        <div class="health__card">
+
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Layout from '@src/components/Layout'
+import CreateModalMixin from '@src/mixins/create-modal'
+
+import { createNamespacedHelpers } from 'vuex'
+const { mapMutations: mapSystemMutations } = createNamespacedHelpers('system')
+
+const category = 'identity/kyc/AU'
+
+export default {
+  name: 'home',
+  mixins: [
+    CreateModalMixin
+  ],
+  components: {
+    Layout
+  },
+  data () {
+    return {
+      collections: [category],
+      navigation: [
+        'Note',
+        'Prescription',
+        'Inbox'
+      ]
+    }
+  },
+  beforeMount () {
+    this.setActions(this.navigation)
+  },
+  methods: {
+    ...mapSystemMutations([ 'setActions' ]),
+    createVerification () {
+      this.showModal(category)
+    }
+  }
+}
+</script>
