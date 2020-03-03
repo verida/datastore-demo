dc00ede3531da4cce144ee3c49179e0ae9c98344
antonvakulenko93 <anton.vakulenko93@gmail.com>
2020-03-03 19:10:45 +0200

    added the validation reset

--- projects/receipt/components/modals/RequestRecipient.vue
+++ projects/receipt/components/modals/RequestRecipient.vue
@@ -4,7 +4,7 @@
              content-class="recipient-modal"
              header-class="recipient-modal__header">
         <div class="recipient-modal__content">
-            <ValidationObserver v-slot="{ invalid }">
+            <ValidationObserver v-slot="{ invalid }" ref="validator">
                 <ValidationProvider v-slot="{ errors }" rules="required|did">
                     <b-form-textarea v-model="did" name="did"
                                      spellcheck="false"
@@ -38,7 +38,7 @@ export default {
     data () {
         return {
             placeholder: 'did:ethr:0x57127C0C0b891125af4441a51BF37F465cDb9d73',
-            did: 'did:ethr:0x57127C0C0b891125af4441a51BF37F465cDb9d73',
+            did: null,
             shown: false
         }
     },
@@ -52,7 +52,8 @@ export default {
     watch:{
         shown () {
             if (this.shown) {
-                //this.did = null
+                this.did = null
+                this.$refs.validator.reset()
             }
         }
     }
